25bbe2b8f4de28eee39fc5b90455253904d18e64
Desire <desireirankundawilliams@gmail.com>
2022-07-06 20:43:07 +0200

    able to display details

---
+++ src/Component/Detail.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Grid,Typography } from '@material-ui/core'
+import PropTypes from "prop-types";
+
+const Detail = (props) => {
+    return (
+        <>
+            <Grid item sm={12} md={6}>
+                <img width={600} height={480} src={props.image} alt={props.title} />
+            </Grid>
+            <Grid style={{textAlign: 'left'}} item xs={12} sm={6}>
+                <h2>{props.title} </h2>
+                <Typography  variant="body2" gutterBottom>
+                description:{props.description}
+                    </Typography>
+                
+                <Typography  variant="body1" gutterBottom>price:{props.price} </Typography>
+            </Grid>
+        </>
+    )
+}
+
+Detail.propTypes = {
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+};
+
+export default Detail
--- src/Pages/ProductDetail.jsx
+++ src/Pages/ProductDetail.jsx
@@ -1,22 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { Container } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Detail from '../Component/Detail';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
-}));
 
 const ProductDetail = () => {
-    const classes = useStyles();
     const [productDetail, setProductDetail] = useState([])
 
     const getProductDetail = () => {
@@ -36,14 +24,12 @@ const ProductDetail = () => {
         <Container>
             <br />
             <Grid container spacing={3}>
-                <Grid item sm={12} md={6}>
-                    <img width={640} height={480} src={productDetail.images} />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <h2>{productDetail.title} </h2>
-                    <h2>description:{productDetail.description} </h2>
-                    <h2>price:{productDetail.price} </h2>
-                </Grid>
+            <Detail 
+            image={productDetail.images}
+            title={productDetail.title}
+            description={productDetail.description}
+            price={productDetail.price}
+            />
             </Grid>
         </Container>
 
